143cfe4c3546e1faa7542d1a9ee0526552621080
alfredochs <chanta.alfredo@gmail.com>
2022-03-22 18:07:36 +0100

    layout portfolio

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,116 +1,71 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+  <div class="container bg-dark text-white">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark mt-2">
+      <div class="d-flex align-items-center justify-items-between">
+        <div>
+          <a class="navbar-brand" href="#">Portfolio</a>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Features</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Pricing</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled">Disabled</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div class="row">
+      <div class="col-6 bg-dark m-auto">
+        <!-- Text Container -->
+        <div class="container d-flex flex-column p-0 align-items-baseline">
+          <span class="d-block">Ciao, sono</span>
+          <span class="my-name">Alfredo</span>
+          <span class="my-name">Chanta Solis</span>
+          <span
+            >A young UI / UX designer appasinate for web & mobile design.</span
+          >
+          <span>Full Stack Developer.</span>
+        </div>
+        <div class="">
+          <ul class="list-unstyled d-flex">
+            <li><font-awesome-icon icon="fa-solid fa-user-secret" /></li>
+            <li>Icona 1</li>
+            <li>Icona 2</li>
+            <li>Icona 3</li>
+            <li>Icona 4</li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-6">
+        <img src="../../src/assets/images/cv-photo-2.jpg" alt="" class="w-50">
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: "HelloWorld",
-  props: {
-    msg: String,
-  },
-};
+  export default {
+    name: "HelloWorld",
+    props: {
+      msg: String
+    }
+  };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-//
 <style scoped lang="scss">
-// h3 {
-//   margin: 40px 0 0;
-// }
-// ul {
-//   list-style-type: none;
-//   padding: 0;
-// }
-// li {
-//   display: inline-block;
-//   margin: 0 10px;
-// }
-// a {
-//   color: #42b983;
-// }
-//
+  .my-name {
+    font-size: 60px;
+    color: red;
+    display: flex;
+    flex-wrap: wrap;
+  }
 </style>
