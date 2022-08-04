e0d5d35211797626aa08c8ade006eb66f277193b
alfredochs <chanta.alfredo@gmail.com>
2022-08-04 16:55:19 +0200

    fix order

--- src/components/partials_pokedex/Pokemons.vue
+++ src/components/partials_pokedex/Pokemons.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="container">
     <div class="row row-cols-6 g-4">
-      <div class="col" v-for="pokemon, i in pokemonsAllInfo" :key="i">
+      <div class="col" v-for="pokemon, i in pokemons" :key="i">
         <div class="card h-100">
-          <img :src="pokemon.img" class="card-img-top" alt="...">
+          <img :src="pokemon.image" class="card-img-top" alt="...">
           <div class="card-body">
             <h5 class="card-title">{{ pokemon.name }}</h5>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
           </div>
         </div>
       </div>
@@ -22,79 +21,89 @@ export default {
       apiGenerations: "https://pokeapi.co/api/v2/generation/",
       apiGeneral: "https://pokeapi.co/api/v2/pokemon/",
       pokemons: [],
-      pokemonsIds: [],
-      pokemonsAllInfo: [],
     };
   },
   props: {
     generationID: [String, Number],
-    generationCount: Number,
   },
   methods: {
-    async getAllPokemonsNameAndUrl() {
-      await axios.get(this.apiGenerations + this.generationID).then((resp) => {
-        this.pokemons = resp.data.pokemon_species;
-      });
-    },
-    async getIdsForAllPokemons() {
-      this.pokemons.forEach((el) => {
-        const arr = el.url.split("/");
-        const id = arr[arr.length - 2];
-        this.pokemonsIds.push(id);
-      });
-      return this.pokemonsIds.sort((a, b) => {
-        return a - b;
-      });
-    },
-
-    async getAllDataSinglePokemon() {
-      const endPoints = [];
-      this.pokemonsIds.forEach((id) => {
-        const endPoint = this.apiGeneral + id;
-        endPoints.push(endPoint);
-      });
-      axios.all(endPoints.map((endPoint) => {
-        axios.get(endPoint).then(resp => {
-          const pathImg = resp.data.sprites.other.dream_world.front_default;
-          // const anotherPath = resp.data.sprites.other.official-artwork.front_default
-          const customPokemonData = {
-            'id': resp.data.id,
-            'name': resp.data.name,
-            'img': pathImg ? pathImg : 'anotherPath'
-          };
-          this.pokemonsAllInfo.push(customPokemonData);
-          // this.pokemonsAllInfo = resp.data
-        });
-      }));
-    },
-    /**
-     * Will order the passed array
-     * @param {Array} arr 
-     */
-    orderById(arr) {
-      return new Promise((resolve) => {
-        arr.sort((a, b) => {
-          return a.id - b.id;
+    fetchData() {
+      return new Promise((resolve, reject) => {
+        axios.get(this.apiGenerations + this.generationID).then((resp) => {
+          this.pokemons = resp.data.pokemon_species;
+        }).then(() => {
+          this.pokemons.forEach(pokemon => {
+            pokemon.id = pokemon.url.split("/").filter((el) => { return !!el; }).pop();
+            // axios.get(this.apiGeneral+pokemon.id).then((resp) =>{
+            // const dreamImage = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`;
+            // const defaultImage = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`;
+            const officialArtwork = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`
+            pokemon.image = officialArtwork;
+            // })
+          });
+          this.pokemons.sort((a, b) => {
+            return a.id - b.id;
+          });
         });
         resolve();
+        reject('error');
       });
     },
-
-    async getAll() {
-      await this.getAllPokemonsNameAndUrl();
-      await this.getIdsForAllPokemons();
-      await this.getAllDataSinglePokemon();
-      await this.orderById(this.pokemonsAllInfo);
-    },
   },
-  computed: {},
   created() {
-    this.getAll();
+    this.fetchData();
   },
   mounted() {
+    // this.order();
   },
 };
 </script>
 
 <style>
 </style>
+
+<!-- // async getAllPokemonsNameAndUrl() {
+    //   await axios.get(this.apiGenerations + this.generationID).then((resp) => {
+    //     this.pokemons = resp.data.pokemon_species;
+    //   });
+    // },
+    // async getIdsForAllPokemons() {
+    //   this.pokemons.forEach((el) => {
+    //     const arr = el.url.split("/");
+    //     const id = arr[arr.length - 2];
+    //     this.pokemonsIds.push(id);
+    //   });
+    //   return this.pokemonsIds.sort((a, b) => {
+    //     return a - b;
+    //   });
+    // },
+    // async getAllDataSinglePokemon() {
+    //   return new Promise((resolve, reject) => {
+    //     const endPoints = [];
+    //     // const finalArr = [];
+    //     this.pokemonsIds.forEach((id) => {
+    //       const endPoint = this.apiGeneral + id;
+    //       endPoints.push(endPoint);
+    //     });
+
+    //     axios.all(endPoints.map((endPoint) => {
+    //       axios.get(endPoint).then(resp => {
+    //         const pathImg = resp.data.sprites.other.dream_world.front_default;
+    //         // const anotherPath = resp.data.sprites.other.official-artwork.front_default
+    //         const customPokemonData = {
+    //           'id': resp.data.id,
+    //           'name': resp.data.name,
+    //           'img': pathImg ? pathImg : 'anotherPath'
+    //         };
+    //         this.pokemonsAllInfo.push(customPokemonData);
+    //       });
+    //     }));
+    //     resolve(this.pokemonsAllInfo);
+    //     reject('err');
+    //   });
+    // },
+    // async order() {
+    //   return this.pokemonsAllInfo.sort((a, b) => {
+    //     return a.id - b.id;
+    //   });
+    // }, -->
--- src/views/Pokedex.vue
+++ src/views/Pokedex.vue
@@ -1,22 +1,13 @@
 <template>
   <div class="container">
-    <!-- <div class="py-3 d-flex justify-content-center">
-      <div class="border">
-        <ul>
-          <li v-for="(generation, i) in infoFromGenerations" :key="i">
-            {{ generation.name }}
-          </li>
-        </ul>
-      </div>
-    </div> -->
     <div class="row row-cols-1 row-cols-md-2 g-4 my-auto">
-      <div v-for="generation, i in infoFromGenerations" :key="i" class="col">
+      <div v-for="generation, i in generationsData" :key="i" class="col">
         <div class="card">
           <!-- <img src="..." class="card-img-top" alt="..." /> -->
           <div class="card-body">
             <h5 class="card-title">{{ generation.name.toUpperCase() }}</h5>
-            <p>Quantity Of Pokemons Founded: <span class="badge bg-info">{{ generation.pokemon_quantity }}</span></p>
-            <a class="btn btn-primary" :href="generation.url">Take data from this generation.</a>
+            <p>Quantity Of Pokemons Founded: <span class="badge bg-info">{{ generation.count }}</span></p>
+            <!-- <a class="btn btn-primary" :href="generation.url">Take data from this generation.</a> -->
             <router-link type="button" class="btn btn-outline-primary" :to="{
               name: 'Pokemons',
               params: {
@@ -41,77 +32,33 @@ export default {
       apiGeneral: "https://pokeapi.co/api/v2/pokemon/",
       apiGenerations: "https://pokeapi.co/api/v2/generation/",
       generationsData: [],
-      generationsIds: [],
-      infoFromGenerations: [],
     };
   },
   methods: {
     /**
-     * Return objects with the name(ex:generation-i) and url endpoint.
+     * Return objects with the name(ex:generation-i),url endpoint and id.
      * @param {String} url 
      */
-    getGenerations(url) {
+    fetchGenerations(url) {
       return new Promise((resolve, reject) => {
         axios.get(url).then((resp) => {
           this.generationsData = resp.data.results;
-          resolve();
-          reject("Error");
-        });
-      });
-    },
-    /**
-     * Loop for the generationsData and take the ids for every single 
-     * generation and create a new Array.
-     * In this case the ids are form 1 to 9 at the last of endpoints
-     * But if the ids were differents numbers ? 
-     */
-    getIdsGenerations() {
-      return new Promise((resolve, reject) => {
-        this.generationsData.forEach((generation) => {
-          const urlArr = generation.url.split("/");
-          const index = urlArr[urlArr.length - 2];
-          this.generationsIds.push(index);
-          resolve();
-          reject("Error getIdsGenerations()");
-        });
-      });
-    },
-    /**
-     * Return custom object with useful information about that specific
-     * generation. 
-     */
-    async getInfoByGenerations() {
-      const endPoints = [];
-      this.generationsIds.forEach((id) => {
-        const call = this.apiGenerations + id;
-        endPoints.push(call);
-      });
-      return axios
-        .all(endPoints.map((endPoint) => axios.get(endPoint)))
-        .then((resp) => {
-          resp.map((respObj) => {
-            const usefulInfo = {
-              id: respObj.data.id,
-              name: respObj.data.main_region.name,
-              url: respObj.data.main_region.url,
-              pokemon_quantity: respObj.data.pokemon_species.length,
-            };
-            this.infoFromGenerations.push(usefulInfo);
+        }).then(() => {
+          this.generationsData.forEach((generation) => {
+            generation.id = generation.url.split("/").filter((el) => { return !!el; }).pop();
+            axios.get(generation.url).then((resp) => {
+              generation.count = resp.data.pokemon_species.length;
+              generation.name = resp.data.main_region.name;
+            });
           });
+          resolve();
+          reject("Error in fetchGenerations");
         });
-    },
-    /**
-     * execute all async function ordered
-     */
-    async TakeAll() {
-      await this.getGenerations(this.apiGenerations);
-      await this.getIdsGenerations();
-      await this.getInfoByGenerations();
+      });
     },
   },
   created() {
-    this.TakeAll();
-
+    this.fetchGenerations(this.apiGenerations);
   },
   mounted() {
   },
